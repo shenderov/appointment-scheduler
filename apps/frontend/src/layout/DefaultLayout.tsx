cd071319c634f7cc0378796556d8984e037eac37
shenderov <shenderov.k@gmail.com>
2025-10-29 00:00:49 -0500

    Unify navbar margins with dashboard

--- apps/frontend/src/layout/DefaultLayout.tsx
+++ apps/frontend/src/layout/DefaultLayout.tsx
@@ -2,17 +2,30 @@ import { Outlet } from 'react-router-dom';
 import Navbar from '@layout/components/Navbar';
 import Footer from '@layout/components/Footer';
 import { useAuth } from '@auth/hooks/useAuth';
+import { Box } from '@mui/material';
 
 const DefaultLayout = () => {
   const { user } = useAuth();
 
   return (
     <>
-      <Navbar role={user?.role} />
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+        <Box
+          sx={{
+            zIndex: (theme) => theme.zIndex.drawer + 1,
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: 0,
+          }}
+        >
+          <Navbar role={user?.role} />
+        </Box>
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </Box>
     </>
   );
 };
